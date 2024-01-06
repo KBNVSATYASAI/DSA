5980c11cd8e93880cf6c8ff47d01ba561ebd140f
KARNA.B.N.V.SATYA SAI <133404996+KBNVSATYASAI@users.noreply.github.com>
2024-01-06 18:00:14 +0530

    Add files via upload
    single frequency means the element which repeats only for single time in while loop traversing an array.

---
+++ singlefrequency.java
@@ -0,0 +1,25 @@
+package Arraysbasics;
+
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class singlefrequency {
+
+	public static void main(String[] args) {
+           
+		int size;
+		Scanner sc=new Scanner(System.in);
+		size=sc.nextInt();
+		int[] arr=new int[size];
+		
+		for(int i=0;i<arr.length;i++) {
+			arr[i]=sc.nextInt();
+		}
+		
+            
+            	 
+ } 
+ 	
+
+} 
