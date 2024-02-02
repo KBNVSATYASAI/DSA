b0a4c77d85a4845ecb4921fadda31189ad5eb7ce
KARNA.B.N.V.SATYA SAI <133404996+KBNVSATYASAI@users.noreply.github.com>
2024-02-02 18:29:29 +0530

    Update MaxinArray.java

--- MaxinArray.java
+++ MaxinArray.java
@@ -2,7 +2,7 @@ package Arraysbasics;
 
 import java.util.Scanner;
 
-public class MaxinArray {
+public class MaxxinArray {
 	public static void main(String[] args) {
 		
 		System.out.println("enter array lenth");
